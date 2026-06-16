7ade596394b52bbfd0ad323d7744274d2ef864fa
Rayan <rsharma2_be23@thapar.edu>
2026-06-16 15:48:43 +0530

    tool calling ext

--- tools_/toolCalling.py
+++ tools_/toolCalling.py
@@ -15,16 +15,4 @@ llm = ChatMistralAI(model="mistral-small-2506")
 
 #tool binding
 llm_with_tool = llm.bind_tools([get_length])
-#llm decides the tool
-result=llm_with_tool.invoke("use the get_length tool to find length of following : hello how are you")#result of binded llm
-#execute tool
-if result.tool_calls:
-   tool_call=result.tool_calls[0]
 
-tool_name=tool_call["name"]
-tool_args=tool_call['args']
-
-tool_result=get_length.invoke(tool_args)
-#send back to llm
-final_response=llm_with_tool.invoke(f"lenght of text is {tool_result}")
-print(final_response)
